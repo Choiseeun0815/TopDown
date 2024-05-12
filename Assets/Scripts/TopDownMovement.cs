614bf76bff29c9ee12bcb0ae5acf9bf7d9628846
Choiseeun0815 <salmons11@naver.com>
2024-05-12 21:09:04 +0900

    Update_PlayerMove
    WASD를 사용하여 캐릭터 이동에 대한 기능 구현

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody2D;
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody2D.velocity = direction;
+    }
+}
